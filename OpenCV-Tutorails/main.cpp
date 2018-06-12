d1aea7e0b324e0a62fb45be3d66e90349f4f3d98
Logan Farr <logan@loganfarr.com>
2018-06-11 21:30:20 -0600

    Initial Commit

---
+++ OpenCV-Tutorails/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV-Tutorails
+//
+//  Created by Logan Farr on 6/11/18.
+//  Copyright © 2018 Logan Farr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
